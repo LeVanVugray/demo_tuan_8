e1b4bc05466a18ccf3fe64b274ac98d5f65aa036
LeVanVugray <23211TT4370@mail.tdc.edu.vn>
2024-11-06 22:43:36 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<?php
+// require "config.php";
+// require "models/db.php";
+// require "models/item.php";
+// require "models/author.php";
+// require "models/category.php";
+// $category = new Category;
+// $item = new Item;
+// $getAllCats = $category->getAllCats();
+// foreach ($getAllCats as $value){
+//     echo $value['id'];
+// }
+
+
+
+include "header.php";
+include "slider.php";
+?>
+<!-- Featured News Slider Start -->
+<?php include "featured-news.php" ?>
+<!-- Featured News Slider End -->
+
+<!-- Lastest News -->
+<!-- Tên lạ -->
+<!-- news-sidebar.php || social-sidebar.php -->
+<?php
+// include "social-sidebar.php";
+?>
+<!-- Lastest News End -->
+<!-- Footer Start -->
+<?php
+include "footer.php";
+?>
